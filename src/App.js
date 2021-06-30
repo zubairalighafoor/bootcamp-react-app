e2bebf793321cb749025f1938fc380ed9a73d9ef
Zubair Ali <55566847+zubairalighafoor@users.noreply.github.com>
2021-06-30 23:13:07 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import {useState} from 'react';
+import Count from './Count';
 import './App.css';
-import Dinner from './Dinner';
 function App() {
+  let [count,setCount]=useState(0);
+  let [day,setDay]=useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World From Zubair Hunjrah</h1>
-        <Dinner dishName="Champ"/>
-      </header>
+    <div className={day?'day':'night'}>
+      <h1>Day Time = {day?"Morning":"Night"}</h1>
+      <Count counter={count}/>
+      <button onClick={()=>{setCount(count+1)}}>Add</button>
+      <button onClick={()=>{setDay(!day)}}>Update Day</button>
     </div>
   );
 }
---
+++ src/Count.js
@@ -0,0 +1,8 @@
+import './App.css';
+function Count(props) {
+  return (
+      <h1>Counter Value = {props.counter}</h1>
+  );
+}
+
+export default Count;
